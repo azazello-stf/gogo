137a9d6a8307d40dd75e7a449799a39407a485db
Bulat <botton95@gmail.com>
2021-12-06 11:05:09 +0300

    https://leetcode.com/problems/search-insert-position/submissions/

--- search_insert_position.go
+++ search_insert_position.go
@@ -4,29 +4,39 @@ import "fmt"
 
 func main() {
 	input := []int{1, 3}
-	target := 2
+	target := 3
 	fmt.Println(searchInsert(input, target))
 }
 
 func searchInsert(nums []int, target int) int {
 	length := len(nums)
-	start, end := 0, length
-	key := end / 2
+	end := length - 1
+	position := end / 2
+	step := 1
+	if step > 1 {
+		step = position
+	}
+	prevPosition := length + 1
 	for {
-		if key-1 >= 0 && key+1 < length && nums[key-1] < target && nums[key+1] >= target {
-			return key
-		} else if key == length-1 && nums[key] < target {
-			return key + 1
-		} else if key == 0 {
-			return key
+		if position == end && nums[position] < target {
+			return end + 1
+		} else if position+1 <= length-1 && nums[position] < target && nums[position+1] > target {
+			return position + 1
+		} else if position-1 >= 0 && nums[position] > target && nums[position-1] < target {
+			return position
+		} else if prevPosition == position && step <= 1 {
+			return position
 		}
 
-		if nums[key] > target {
-			end = key
-			key = (end - start) / 2
-		} else if nums[key] < target {
-			start = key
-			key = key + (end-start)/2
+		prevPosition = position
+
+		if position > 0 && nums[position] > target {
+			position -= step
+		} else if position+step <= length-1 && nums[position] < target {
+			position += step
+		}
+		if step > 1 {
+			step /= 2
 		}
 	}
 }
